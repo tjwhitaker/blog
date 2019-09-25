17ba97fcff32d2652ca366ecc6dd56e96c2f2ced
Tim Whitaker <camp4climber@gmail.com>
2019-09-25 13:49:06 -0600

    Copy static folder and copile single template

--- src/main.jl
+++ src/main.jl
@@ -1,5 +1,10 @@
 include("../Stasis.jl/src/Stasis.jl")
 using .Stasis
 
-title = "Odd Prophet"
-Stasis.build("src/pages/index.jl", "build/index.html", title)
+Stasis.build("src/pages/index.jl", "build/index.html")
+
+for (root, dirs, files) in walkdir("src/static")
+  for file in files
+    Stasis.copy(joinpath(root, file), joinpath("build", file))
+  end
+end
