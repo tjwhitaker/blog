5c3e1571668f2fd3b7850a4c799bbe067da2d61d
Tim Whitaker <camp4climber@gmail.com>
2019-10-02 14:30:14 -0600

    Add index content and pass in post data

--- src/main.jl
+++ src/main.jl
@@ -1,7 +1,22 @@
 include("../Stasis.jl/src/Stasis.jl")
 using .Stasis
 
-Stasis.build("src/pages/index.jl", "build/index.html", posts=[1,2,3])
+posts = [
+  Dict(
+    :title => "Hello World",
+    :excerpt => "Lorem",
+    :category => "Article",
+    :date => "10-2-2019"
+  ),
+  Dict(
+    :title => "Lorem Ipsum",
+    :excerpt => "Lorem",
+    :category => "Article",
+    :date => "10-4-2019"
+  )
+]
+
+Stasis.build("src/pages/index.jl", "build/index.html", posts=posts)
 
 for (root, dirs, files) in walkdir("src/static")
   for file in files
--- src/pages/index.jl
+++ src/pages/index.jl
@@ -9,14 +9,43 @@ html(lang="en") do
   end
 
   body() do
-    main() do
-      h1("Hello World")
-      form() do
-        input(type="email", data_netlify="true")
+    header() do
+      div(class="wrapper") do
+        div(class="logo") do
+          h1("Odd Prophet")
+        end
+        
+        nav() do
+          a("Home", href="/")
+        end
       end
+    end
 
-      for post in context[:posts]
-        p(post)
+    div(class="container") do
+      div(class="wrapper") do
+        div(class="grid") do
+          main() do
+            p("""
+              Welcome to Odd Prophet! This is a website to explore cool datasets and machine learning algorithms.
+            """)
+
+            p("""
+              Thanks to increased computing power, machine learning models are helping us glean insights from the vast, complex datasets that describe our universe. From galaxies billions of light years away to particles billions of times smaller than an atom, science experiments are producing more data than ever before. These algorithms are the most powerful way to learn from these experiments and I believe they are the key to unlocking a new data-driven paradigm in science.
+            """)
+
+            img(src="/inception.png")
+          end
+
+          aside() do
+            for post in context[:posts]
+              div() do
+                h3(post[:title])
+                p(post[:excerpt])
+                small(post[:category] * " from " * post[:date])
+              end
+            end
+          end
+        end
       end
     end
   end
