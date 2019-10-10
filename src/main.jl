ce992a350ca0a28abe970d85544d17e2229b3743
Tim Whitaker <camp4climber@gmail.com>
2019-10-10 15:18:40 -0600

    Test new args

--- src/main.jl
+++ src/main.jl
@@ -1,31 +1,37 @@
 include("../Stasis.jl/src/Stasis.jl")
 using .Stasis
 
-# Collection of post metadata for index
+Stasis.config(
+  source_dir="src",
+  build_dir="build",
+  template_engine="affinity"
+)
+
+# Collection of post metadata for index and blog
 feed = []
 
 # Parse markdown and build articles
-for file in Stasis.walk("src/content")
+for file in Stasis.walk("content")
   meta = Stasis.parse_toml(file)
   content = Stasis.parse_markdown(file)
 
   push!(feed, meta)
 
   Stasis.build(
-    "src/templates/article.jl",
-    "build/blog/$(meta["slug"])/index.html", 
+    input="templates/article.jl",
+    output="blog/$(meta["slug"])/index.html", 
     meta=meta, 
     content=content
   )
 end
 
 # Build static pages
-Stasis.build("src/templates/index.jl", "build/index.html", posts=feed)
-Stasis.build("src/templates/blog.jl", "build/blog/index.html", posts=feed)
-Stasis.build("src/templates/404.jl", "build/404.html")
+Stasis.build(input="templates/index.jl", output="index.html", posts=feed)
+Stasis.build(input="templates/blog.jl", output="blog/index.html", posts=feed)
+Stasis.build(input="templates/404.jl", output="404.html")
 
 # Copy static files
-Stasis.copy("src/static", "build/static")
+Stasis.copy("static")
 
 # Start dev server
-Stasis.serve("build")
+Stasis.serve()
