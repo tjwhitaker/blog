b1b344d06794999cf2d97b75387dc0d1f4c0b146
Tim Whitaker <tmwhtkr@gmail.com>
2019-10-11 10:52:05 -0600

    Use kwargs for api calls

--- src/main.jl
+++ src/main.jl
@@ -1,37 +1,31 @@
 include("../Stasis.jl/src/Stasis.jl")
 using .Stasis
 
-Stasis.config(
-  source_dir="src",
-  build_dir="build",
-  template_engine="affinity"
-)
-
 # Collection of post metadata for index and blog
 feed = []
 
 # Parse markdown and build articles
-for file in Stasis.walk("content")
+for file in Stasis.walk("src/content")
   meta = Stasis.parse_toml(file)
   content = Stasis.parse_markdown(file)
 
   push!(feed, meta)
 
   Stasis.build(
-    input="templates/article.jl",
-    output="blog/$(meta["slug"])/index.html", 
+    template="src/templates/article.jl",
+    output="build/blog/$(meta["slug"])/index.html", 
     meta=meta, 
     content=content
   )
 end
 
 # Build static pages
-Stasis.build(input="templates/index.jl", output="index.html", posts=feed)
-Stasis.build(input="templates/blog.jl", output="blog/index.html", posts=feed)
-Stasis.build(input="templates/404.jl", output="404.html")
+Stasis.build(template="src/templates/index.jl", output="build/index.html", posts=feed)
+Stasis.build(template="src/templates/blog.jl", output="build/blog/index.html", posts=feed)
+Stasis.build(template="src/templates/404.jl", output="build/404.html")
 
 # Copy static files
-Stasis.copy("static")
+Stasis.copy(input="src/static", output="build/static")
 
 # Start dev server
-Stasis.serve()
+Stasis.serve("build")
