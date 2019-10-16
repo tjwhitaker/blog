0741ce78e45a9926dfde8d96ea9429e20924a556
Tim Whitaker <tmwhtkr@gmail.com>
2019-10-16 15:06:05 -0600

    Add katex support to articles

--- src/templates/article.jl
+++ src/templates/article.jl
@@ -8,6 +8,9 @@ html(lang="en") do
     link(rel="stylesheet", href="/static/styles/theme.css")
     link(rel="stylesheet", href="/static/styles/article.css")
     link(rel="stylesheet", href="https://fonts.googleapis.com/css?family=Fira+Mono:400,500,700|Fira+Sans:400,500,700&display=swap")
+    link(rel="stylesheet", href="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css", integrity="sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq", crossorigin="anonymous")
+    
+    script(src="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.js", integrity="sha384-y23I5Q6l+B6vatafAwxRu/0oK/79VlbSz7Q9aiSZUvyWYIYsd+qj+o24G5ZU2zJz", crossorigin="anonymous")
   end
 
   body() do
@@ -44,5 +47,7 @@ html(lang="en") do
     footer() do
       div(class="wrapper")
     end
+
+    script(src="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/contrib/auto-render.min.js", integrity="sha384-kWPLUVMOks5AQFrykwIup5lo0m3iMkkHrD0uJ4H5cjeGihAutqP0yW0J6dpFiVkI", crossorigin="anonymous", onload="renderMathInElement(document.body, {'delimiters': [{left: '\$', right: '\$', display: true}]});")
   end
 end
