eb9ad0906889d2455ac9576bd88fd3d87b4ecd33
Tim Whitaker <tmwhtkr@gmail.com>
2019-10-27 08:11:48 -0600

    Simplify site to home + articles

---
+++ src/content/the-higgs-boson-challenge.md
@@ -0,0 +1,23 @@
++++
+title = "The Higgs Boson Challenge"
+slug = "the-higgs-boson-challenge"
+category = "Article"
+date = "10-21-2019"
+excerpt = "Self Normalizing Neural Networks (SNN) are a new approach to building deep fully connected networks. The SNN suggests a combination of these 4 concepts..."
++++
+
+Self Normalizing Neural Networks (SNN) are a new approach to building deep fully connected networks.
+
+The SNN suggests a combination of these 4 concepts.
+
+1. Normalize input data to mean 0 and variance 1.
+2. Initialize weights for each layer to be sampled from a normal distribution of mean 0 and variance 1/n where n is the number of weights in that layer.
+3. Apply the Scaled Exponential Linear Unit (SELU) activation function to fully connected layers.
+4. Apply Alpha Dropout layers after every dense layer.
+
+```math
+SELU(x) = \begin{cases}
+   \lambda x &\text{if } x < 0 \\
+   \lambda \alpha e^x - \lambda \alpha &\text{if } x >= 0
+\end{cases}
+```
--- src/templates/index.jl
+++ src/templates/index.jl
@@ -19,10 +19,12 @@ html(lang="en") do
           end
           
           nav() do
-            a("About", href="/about")
-            a("Blog", href="/blog")
-            a("Contact", href="/contact")
-            a("Home", href="/")
+            a(raw"""
+              <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="home" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="svg-inline--fa fa-home fa-w-18 fa-3x"><path fill="currentColor" d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z" class=""></path></svg>
+            """, href="/")
+            a(raw"""
+              <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="code" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="svg-inline--fa fa-code fa-w-20 fa-3x"><path fill="currentColor" d="M278.9 511.5l-61-17.7c-6.4-1.8-10-8.5-8.2-14.9L346.2 8.7c1.8-6.4 8.5-10 14.9-8.2l61 17.7c6.4 1.8 10 8.5 8.2 14.9L293.8 503.3c-1.9 6.4-8.5 10.1-14.9 8.2zm-114-112.2l43.5-46.4c4.6-4.9 4.3-12.7-.8-17.2L117 256l90.6-79.7c5.1-4.5 5.5-12.3.8-17.2l-43.5-46.4c-4.5-4.8-12.1-5.1-17-.5L3.8 247.2c-5.1 4.7-5.1 12.8 0 17.5l144.1 135.1c4.9 4.6 12.5 4.4 17-.5zm327.2.6l144.1-135.1c5.1-4.7 5.1-12.8 0-17.5L492.1 112.1c-4.8-4.5-12.4-4.3-17 .5L431.6 159c-4.6 4.9-4.3 12.7.8 17.2L523 256l-90.6 79.7c-5.1 4.5-5.5 12.3-.8 17.2l43.5 46.4c4.5 4.9 12.1 5.1 17 .6z" class=""></path></svg>
+            """, href="https://github.com/OddProphet", target="_blank")
           end
         end
       end
