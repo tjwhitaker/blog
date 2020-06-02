2dd5146682c62418f900328eb59c37c5d6610f9c
Tim Whitaker <camp4climber@gmail.com>
2020-06-02 16:39:20 -0600

    Fix katex renders

--- src/templates/article.jl
+++ src/templates/article.jl
@@ -33,6 +33,14 @@ html(lang="en") do
       div(class="wrapper")
     end
 
-    script(src="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/contrib/auto-render.min.js", integrity="sha384-kWPLUVMOks5AQFrykwIup5lo0m3iMkkHrD0uJ4H5cjeGihAutqP0yW0J6dpFiVkI", crossorigin="anonymous", onload="renderMathInElement(document.body, {'delimiters': [{left: '\$\$', right: '\$\$', display: true}, {left: '\$', right: '\$', display: false}]});")
+      script() do
+          raw("""
+            var math = document.getElementsByClassName('math');
+            for (var i = 0; i < math.length; i++) {
+              katex.render(math[i].textContent, math[i], {displayMode: math[i].classList.contains('display')});
+            }
+          """)
+      end
+
   end
 end
